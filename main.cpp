c35b0d7693b8564ea9ad1551597dfbeaf1ea1269
torisambrook <tori.sambrook@gmail.com>
2020-03-18 14:46:05 -0600

    Updating printMap function

--- main.cpp
+++ main.cpp
@@ -27,35 +27,51 @@ function to damage player or monster
 #include <string>
 using namespace std;
 
-struct player
+#define COLUMNS 61
+
+struct Player
 {
     int row = 0;
     int column = 0;
-    int health = 50;
+    bool alive = true;
 };
 
 struct Monster 
 {
     int row = 0;
     int column = 0;
-    int health = 0;
 };
 
+Player Player1;
 void printMap(string);
+void findPlayer(Player&);
+
+
 
 int main()
 {
-    string levels[3] = {"level1.txt", "level2.txt", "level3.txt"};
-    printMap(levels[0]);
+    
+    string levelsList[3] = {"level1.txt", "level2.txt", "level3.txt"};
+    printMap(levelsList[0]);
+    
+    // while the monsters haven't reached the player, ask user for next player move. Move the player and monsters. 
+    // if monster is adjacent to player than game over and player loses. else if player has reached '@' then move to next level. 
+    // else if player has reached '*' then player wins and game over. Print level at end of each loop. 
+
+    findPlayer(Player1);
 
     return 0;
+}
+void findPlayer(Player& player1)
+{
+    
 }
 
 // Function printMap opens the level file, and prints it to the screen.
 void printMap(string level)
 {
     int map, item;
-    char buffer[60];
+    char buffer[COLUMNS];
     
     map = open(level.c_str(), O_RDONLY, S_IRUSR); 
     if(map == -1)
@@ -63,9 +79,34 @@ void printMap(string level)
         perror("\nomap file open errror: ");
         exit(1);
     }
-    while( (item=read(map, buffer, 60))!=0)
+    int row = 0;
+    int size = COLUMNS;
+    // For each row of characeters in the map, execute the read() system call
+   while((item=read(map, buffer, size))!=0)
     {
-            item=write(1,buffer,item);
+        if(row == 0)
+            size++;
+        // Update Player location and map format
+       // cout << "Items read: " << item;
+        row++;
+        for(int i = 0; i < COLUMNS; i++)
+        {
+            if(buffer[i] == 'P')
+            {
+                Player1.row = row;
+                Player1.column = i;
+            }
+            else if (buffer[i] == '.')
+            {
+                buffer[i] == ' ';
+            }
+        }
+        
+        // Print the map to the screen using the write() system call
+        item = write(1, buffer, item);
+        //cout << "Items written: " << item;
     }
+
+    cout << endl << "Number of rows: " << row << endl << "Player position: " << Player1.row <<  ", " << Player1.column << endl;
     close (map);
 }
